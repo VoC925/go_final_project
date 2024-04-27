8567339aa72d7dc4fcc47ba84fcd36f98f0a3bb1
Eugene <ewg.covaleov1999@yandex.ru>
2024-04-28 00:34:13 +0300

    начало реализации функции NextDate

--- service/internal/api/handler.go
+++ service/internal/api/handler.go
@@ -2,8 +2,12 @@ package api
 
 import (
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/pkg/errors"
 )
 
 const (
@@ -33,4 +37,73 @@ func (h *handleUser) Register(route *chi.Mux) {
 func (h *handleUser) getHTMLPage(w http.ResponseWriter, req *http.Request) {
 	fs := http.FileServer(http.Dir(pathToHTMLFile))
 	http.StripPrefix("/", fs).ServeHTTP(w, req)
+}
+
+var (
+	ErrEmptyField  = errors.New("задано пустое поле")
+	ErrInvalidData = errors.New("некорректное поле")
+)
+
+type repeatTasks struct {
+	typeRepeat string
+	period     int
+}
+
+func nextDate(now time.Time, date string, repeat string) (string, error) {
+	// валидация repeat
+	if repeat == "" {
+		return "", errors.Wrap(ErrEmptyField, "repeat")
+	}
+	repeatVal, ok := validateReapeatValue(repeat)
+	if !ok {
+		return "", errors.Wrap(ErrInvalidData, "repeat")
+	}
+	// валидация date
+	timeDate, err := time.Parse("20060102", date)
+	if err != nil {
+		return "", errors.Wrap(err, ErrInvalidData.Error())
+	}
+
+	switch repeatVal.typeRepeat {
+	case "d":
+		for {
+			timeDate.AddDate(0, 0, repeatVal.period)
+			if now.After(timeDate) {
+				return timeDate.Format("20060102"), nil
+			}
+		}
+	case "y":
+		for {
+			timeDate.AddDate(repeatVal.period, 0, 0)
+			if now.After(timeDate) {
+				return timeDate.Format("20060102"), nil
+			}
+		}
+	}
+	return "", errors.Wrap(err, ErrInvalidData.Error())
+}
+
+func validateReapeatValue(repeat string) (*repeatTasks, bool) {
+	parts := strings.Split(repeat, " ")
+	switch parts[0] {
+	case "d":
+		if len(parts) != 2 {
+			return nil, false
+		}
+		repeat_days, err := strconv.Atoi(parts[1])
+		if err != nil || repeat_days > 400 {
+			return nil, false
+		}
+		return &repeatTasks{
+			typeRepeat: parts[0],
+			period:     repeat_days,
+		}, true
+	case "y":
+		return &repeatTasks{
+			typeRepeat: parts[0],
+			period:     1,
+		}, true
+	default:
+		return nil, false
+	}
 }
